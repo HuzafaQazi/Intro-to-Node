d9a930085cb2a13305b7ead65a08968bf2fcb178
HuzafaQazi <mohammedhuzafa20@gmail.com>
2019-08-30 21:57:11 +0530

    updated package.json procfile and hello.js PORT

--- hello.js
+++ hello.js
@@ -1,10 +1,14 @@
 var http=require('http');
 var fs=require('fs');
 var url=require('url');
+const PORT=process.env.PORT || 50000
+
+
 http.createServer(function (req,res) {
 	var q=url.parse(req.url,true);
 	var filename='.'+q.pathname;
 	if(filename=='./'){filename='index';}
+
 	filename=filename+".html";
 	fs.readFile(filename,function(err,data){
 		if(err){
@@ -15,4 +19,4 @@ http.createServer(function (req,res) {
 	return res.end(data);
 })
 	// body...
-}).listen(8080);
+}).listen(PORT);
