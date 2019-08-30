e347664d7c0feab10f66af71278e418caf250558
HuzafaQazi <mohammedhuzafa20@gmail.com>
2019-08-30 20:08:31 +0530

    Initial Commit

---
+++ hello.js
@@ -0,0 +1,18 @@
+var http=require('http');
+var fs=require('fs');
+var url=require('url');
+http.createServer(function (req,res) {
+	var q=url.parse(req.url,true);
+	var filename='.'+q.pathname;
+	if(filename=='./'){filename='index';}
+	filename=filename+".html";
+	fs.readFile(filename,function(err,data){
+		if(err){
+			res.writeHead(404,{'content-Type': 'text/html'});
+			return res.end('<strong>404 Not Found</strong>');
+		}
+	res.writeHead(200,{'content-Type': 'text/html'});
+	return res.end(data);
+})
+	// body...
+}).listen(8080);
